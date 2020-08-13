58a2695805e71d9573dc52996f6becb7166e41c8
Roman <j@tjo.biz>
2020-08-13 23:41:26 +0800

    Laravel package of Google API PHP client

--- src/GoogleAPIServiceProvider.php
+++ src/GoogleAPIServiceProvider.php
@@ -1,1 +1,20 @@
 <?php
+namespace GoogleAPI;
+
+use Illuminate\Support\ServiceProvider as MainServiceProvider;
+
+class GoogleAPIServiceProvider extends MainServiceProvider {
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/path/to/config/courier.php' => config_path('courier.php'),
+        ]);
+    }
+
+}
